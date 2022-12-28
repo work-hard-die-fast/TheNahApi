49720ef69c0cb0ee5329c9c0568a1695dbb6da2b
vu pham <work-hard-die-fast@outlook.com>
2022-12-28 20:33:11 +0700

    [refactor] format code

--- src/bootstrap/Server.ts
+++ src/bootstrap/Server.ts
@@ -1,18 +1,18 @@
-import express, {Application} from "express";
-import * as http from "http";
-import {MariadbConnection} from "../config";
+import express, {Application} from "express"
+import * as http from "http"
+import {MariadbConnection} from "../config"
 
 class Server
 {
-   private readonly serverPort: number;
-   private readonly app: Application;
-   private readonly httpServer: http.Server;
+   private readonly serverPort: number
+   private readonly app: Application
+   private readonly httpServer: http.Server
 
    constructor()
    {
-      this.app = express();
-      this.serverPort = 3000;
-      this.httpServer = http.createServer(this.app);
+      this.app = express()
+      this.serverPort = 3000
+      this.httpServer = http.createServer(this.app)
    }
 }
 
--- src/config/RouterConfig.ts
+++ src/config/RouterConfig.ts
@@ -1,5 +1,5 @@
 const RouterConfig = [
    ""
-];
+]
 
 export {RouterConfig}
