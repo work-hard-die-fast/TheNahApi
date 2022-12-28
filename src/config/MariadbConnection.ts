82314097f605dce56e8c6fee5e28fe795d8a3b92
vu pham <work-hard-die-fast@outlook.com>
2022-12-28 20:16:29 +0700

    [feat] create MariadbConnection

---
+++ src/config/MariadbConnection.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata"
+import {DataSource} from "typeorm"
+
+const MariadbConnection = new DataSource({
+   type: "mariadb",
+   host: "",
+   port: 3306,
+   username: "",
+   password: "",
+   database: "",
+   synchronize: true,
+   logging: false,
+   entities: [],
+   migrations: [],
+   subscribers: [],
+})
+
+export {MariadbConnection}
---
+++ src/config/index.ts
@@ -0,0 +1,1 @@
+export {MariadbConnection} from "./MariadbConnection"
