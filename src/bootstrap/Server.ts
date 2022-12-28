466ca999f7c883e44e76aafe89053f50e289e80e
vu pham <work-hard-die-fast@outlook.com>
2022-12-28 20:43:58 +0700

    [feat] create connectMariadb function

--- src/bootstrap/Server.ts
+++ src/bootstrap/Server.ts
@@ -14,6 +14,18 @@ class Server
       this.serverPort = 3000
       this.httpServer = http.createServer(this.app)
    }
+   private async connectDatabase(): Promise<void>
+   {
+      await MariadbConnection.initialize()
+         .then(() => {
+            console.log("--- connect mariadb successfully")
+         })
+         .catch((error) => {
+            console.log("--- mariadb is dump")
+            console.error(error)
+            console.log("------------------------")
+         })
+   }
 }
 
 export {Server}
