e676e6494e01ba2fbeb61b5bb2fd866601c1216a
vu pham <work-hard-die-fast@outlook.com>
2022-12-28 20:32:33 +0700

    [feat] config Server

--- src/bootstrap/Server.ts
+++ src/bootstrap/Server.ts
@@ -1,6 +1,19 @@
+import express, {Application} from "express";
+import * as http from "http";
+import {MariadbConnection} from "../config";
+
 class Server
 {
-   
+   private readonly serverPort: number;
+   private readonly app: Application;
+   private readonly httpServer: http.Server;
+
+   constructor()
+   {
+      this.app = express();
+      this.serverPort = 3000;
+      this.httpServer = http.createServer(this.app);
+   }
 }
 
 export {Server}
