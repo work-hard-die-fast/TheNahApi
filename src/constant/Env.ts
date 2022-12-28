0d04929f37561f0ac52284f078c7aae8d995c9c3
vu pham <work-hard-die-fast@outlook.com>
2022-12-28 21:12:34 +0700

    [feat] add env variables

--- src/constant/Env.ts
+++ src/constant/Env.ts
@@ -1,7 +1,19 @@
 require("dotenv").config()
 
 const Env = Object.freeze({
+   // server
+   serverPort: process.env.SERVER_PORT as any,
 
+   // mariadb
+   dbType: process.env.DB_TYPE as any,
+   dbHost: process.env.DB_HOST as any,
+   dbPort: process.env.DB_PORT as any,
+   dbUsername: process.env.DB_USERNAME as any,
+   dbPassword: process.env.DB_PASSWORD as any,
+   dbName: process.env.DB_NAME as any,
+
+   // jsonwebtoken
+   jwtSecret: process.env.JWT_SECRET as any
 })
 
 export {Env}
