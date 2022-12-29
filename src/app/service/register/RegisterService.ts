0112373e64bbaa04c7b5974ea160fda4164f847b
vu pham <work-hard-die-fast@outlook.com>
2022-12-29 10:25:33 +0700

    [feat] add injectable

--- src/app/service/register/RegisterService.ts
+++ src/app/service/register/RegisterService.ts
@@ -1,8 +1,10 @@
 import {IRegisterService} from "./IRegisterService"
+import {injectable} from "tsyringe"
 
+@injectable()
 class RegisterService implements IRegisterService
 {
-
+   constructor() {}
 }
 
 export {RegisterService}
