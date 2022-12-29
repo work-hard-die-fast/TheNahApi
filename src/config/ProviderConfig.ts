a2b42eaea6a00d225fcf7fb9b1a146c4ba01c868
vu pham <work-hard-die-fast@outlook.com>
2022-12-29 09:50:02 +0700

    [feat] add token for RegisterService

--- src/config/ProviderConfig.ts
+++ src/config/ProviderConfig.ts
@@ -1,5 +1,11 @@
+import {InjectToken} from "../constant"
+import {RegisterService} from "../app/service"
+
 const ProviderConfig = [
-   ""
+   {
+      token: InjectToken.RegisterService,
+      useClass: RegisterService
+   }
 ]
 
 export {ProviderConfig}
