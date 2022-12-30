7c517dc19d0a0cc37d2e815eb6f59b831e2c3857
vu pham <work-hard-die-fast@outlook.com>
2022-12-30 21:47:17 +0700

    [feat] inject UserRepository

--- src/app/service/register/RegisterService.ts
+++ src/app/service/register/RegisterService.ts
@@ -2,15 +2,19 @@ import {IRegisterService} from "./IRegisterService"
 import {inject, injectable} from "tsyringe"
 import {InjectToken} from "../../../constant"
 import {IJwtHelper, JwtHelper} from "../../helper"
+import {IUserRepository} from "../../repository";
 
 @injectable()
 class RegisterService implements IRegisterService
 {
+   private readonly userRepo: IUserRepository
    private readonly jwtHelper: IJwtHelper
    
    constructor(
+      @inject(InjectToken.UserRepository) userRepo: IUserRepository,
       @inject(InjectToken.JwtHelper) jwtHelper: JwtHelper
    ) {
+      this.userRepo = userRepo
       this.jwtHelper = jwtHelper
    }
 }
