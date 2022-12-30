bbb133302605bfcdf3160bc0314d3b804adc41f0
vu pham <work-hard-die-fast@outlook.com>
2022-12-30 21:35:47 +0700

    [feat] create UserRepository

---
+++ src/app/repository/user/UserRepository.ts
@@ -0,0 +1,13 @@
+import {BaseTypeormRepository} from "../../../@base"
+import {IUserRepository} from "./IUserRepository";
+import {User} from "../../model";
+
+class UserRepository extends BaseTypeormRepository<UserRepository> implements IUserRepository
+{
+   constructor()
+   {
+      super(User)
+   }
+}
+
+export {UserRepository}
