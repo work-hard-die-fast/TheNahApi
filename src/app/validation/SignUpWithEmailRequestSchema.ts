03dd364e58b865b6961163ece07ee580b5511d75
vu pham <work-hard-die-fast@outlook.com>
2022-12-30 22:40:54 +0700

    [fix] make abortEarly false
    this allow joi show all error messages

--- src/app/validation/SignUpWithEmailRequestSchema.ts
+++ src/app/validation/SignUpWithEmailRequestSchema.ts
@@ -1,9 +1,11 @@
 import Joi from "joi"
 
-const SignUpWithEmailRequestSchema = Joi.object({
-   username: Joi.string().required(),
-   email: Joi.string().email().required(),
-   password: Joi.string().min(8)
-})
+const SignUpWithEmailRequestSchema = Joi
+   .object({
+      username: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(8)
+   })
+   .options({abortEarly: false})
 
 export {SignUpWithEmailRequestSchema}
