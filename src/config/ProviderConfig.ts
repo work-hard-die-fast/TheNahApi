3d9fc045a12604138fdd8c784317a8b890f1fb86
vu pham <work-hard-die-fast@outlook.com>
2022-12-30 21:04:42 +0700

    [feat] create provider for JwtHelper

--- src/config/ProviderConfig.ts
+++ src/config/ProviderConfig.ts
@@ -1,10 +1,15 @@
 import {InjectToken} from "../constant"
 import {RegisterService} from "../app/service"
+import {JwtHelper} from "../app/helper"
 
 const ProviderConfig = [
    {
       token: InjectToken.RegisterService,
       useClass: RegisterService
+   },
+   {
+      token: InjectToken.JwtHelper,
+      useClass: JwtHelper
    }
 ]
 
