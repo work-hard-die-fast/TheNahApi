68e8b55f60dccc8195cfa050a6a77af89c610779
vu pham <work-hard-die-fast@outlook.com>
2022-12-30 22:28:53 +0700

    [feat] create SignUpWithEmailRequestSchema

---
+++ src/app/validation/SignUpWithEmailRequestSchema.ts
@@ -0,0 +1,9 @@
+import Joi from "joi"
+
+const SignUpWithEmailRequestSchema = Joi.object({
+   username: Joi.string().required(),
+   email: Joi.string().email().required(),
+   password: Joi.string().min(8)
+})
+
+export {SignUpWithEmailRequestSchema}
---
+++ src/app/validation/index.ts
@@ -0,0 +1,1 @@
+export {SignUpWithEmailRequestSchema} from "./SignUpWithEmailRequestSchema"
