3ab880008776bb896cd75cbf1ab6cb8b445fe02a
vu pham <work-hard-die-fast@outlook.com>
2022-12-30 09:50:22 +0700

    [feat] export EmailConfig

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
 export {MariadbConnection} from "./MariadbConnection"
 export {RouterConfig} from "./RouterConfig"
 export {ProviderConfig} from "./ProviderConfig"
+export {EmailConfig} from "./EmailConfig"
