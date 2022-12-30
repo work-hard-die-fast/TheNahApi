154c8d4ab062c9c052d572d7d297efbfb149525e
vu pham <work-hard-die-fast@outlook.com>
2022-12-30 21:05:55 +0700

    [feat] implement encode function

--- src/app/helper/jwt/JwtHelper.ts
+++ src/app/helper/jwt/JwtHelper.ts
@@ -1,8 +1,13 @@
 import {IJwtHelper} from "./IJwtHelper"
+import {sign, SignOptions} from "jsonwebtoken"
+import {Env} from "../../../constant"
 
 class JwtHelper implements IJwtHelper
 {
-
+   encode(data: any, options?: SignOptions): string
+   {
+      return sign({data: JSON.stringify(data)}, Env.jwtSecret, options)
+   }
 }
 
 export {JwtHelper}
