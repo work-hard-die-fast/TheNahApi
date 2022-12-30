5fd492307ed28f81e16989530b955a0425aad34a
vu pham <work-hard-die-fast@outlook.com>
2022-12-30 21:41:49 +0700

    [feat] create UserRepository provider

--- src/config/ProviderConfig.ts
+++ src/config/ProviderConfig.ts
@@ -1,12 +1,22 @@
 import {InjectToken} from "../constant"
 import {RegisterService} from "../app/service"
 import {JwtHelper} from "../app/helper"
+import {UserRepository} from "../app/repository"
 
 const ProviderConfig = [
+   // service
    {
       token: InjectToken.RegisterService,
       useClass: RegisterService
    },
+
+   // repository
+   {
+      token: InjectToken.UserRepository,
+      useClass: UserRepository
+   },
+
+   // helper
    {
       token: InjectToken.JwtHelper,
       useClass: JwtHelper
