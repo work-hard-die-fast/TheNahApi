c1afdf5c469e40414e951194a689b9bd75c17380
vu pham <work-hard-die-fast@outlook.com>
2022-12-30 22:59:29 +0700

    [fix] make password is required

--- src/app/validation/SignUpWithEmailRequestSchema.ts
+++ src/app/validation/SignUpWithEmailRequestSchema.ts
@@ -4,7 +4,7 @@ const SignUpWithEmailRequestSchema = Joi
    .object({
       username: Joi.string().required(),
       email: Joi.string().email().required(),
-      password: Joi.string().min(8)
+      password: Joi.string().min(8).required()
    })
    .options({abortEarly: false})
 
